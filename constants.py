6e97ef992fb8b3fa39d82f1497e3cbbe45384720
victors3136 <victor.selegean@gmail.com>
2025-06-13 16:35:48 +0300

    Add inference endpoint and index landing page

--- constants.py
+++ constants.py
@@ -2,7 +2,7 @@ import boto3
 
 allowed_extensions = [".m4a"]
 bucket_name = "romanian-asr-data"
-inference_endpoint = "https://postman-echo.com/post"
+inference_endpoint = "https://rz4tz35rzar2pjdz.us-east-1.aws.endpoints.huggingface.cloud"
 max_duration_s = 30
 region_name = "eu-north"
 s3_client = boto3.client("s3", region_name=region_name)
--- main.py
+++ main.py
@@ -1,11 +1,63 @@
-from constants import region_name, allowed_extensions, inference_endpoint
+from constants import allowed_extensions, inference_endpoint
 from fastapi import FastAPI, UploadFile, Form, status
-from fastapi.responses import PlainTextResponse
+from fastapi.responses import PlainTextResponse, HTMLResponse
+from fastapi.staticfiles import StaticFiles
 import uuid
 import httpx
 from utils.utils import extract_file_ext, file_ext_is_valid, is_audio_duration_longer_than_allowed, is_consent_given, store_audio_to_s3
-
 app = FastAPI()
+
+app.mount("/assets", StaticFiles(directory="assets"), name="assets")
+@app.get("/", response_class=HTMLResponse)
+async def index():
+    html_content = """
+                   <!DOCTYPE html>
+                   <html lang="en">
+                   <head>
+                       <meta charset="UTF-8">
+                       <title>Welcome to Epigraph Server</title>
+                       <style>
+                           body {
+                               font-family: Arial, sans-serif;
+                               background-color: #3a3e40;
+                               margin: 0;
+                               padding: 0;
+                               display: flex;
+                               justify-content: center;
+                               align-items: center;
+                               width: auto;
+                           }
+
+                           .container {
+                               text-align: center;
+                               background: #232933;
+                               margin-top: 10px;
+                               border-radius: 12px;
+                               box-shadow: 0 4px 20px rgba(0, 0, 0, 0.1);
+                           }
+
+                           img {
+                               max-width: 60%;
+                               height: auto;
+                               border-radius: 8px;
+                               margin-top: 20px;
+                           }
+
+                           h1 {
+                               color: #d6d6d6;
+                               font-size: 2em;
+                           }
+                       </style>
+                   </head>
+                   <body>
+                   <div class="container">
+                       <img src="/assets/index-photo.png" alt="Welcome Image">
+                       <h1>Welcome to Epigraph Server!</h1>
+                   </div>
+                   </body>
+                   </html> \
+                   """
+    return HTMLResponse(content=html_content)
 
 @app.post("/upload-audio/")
 async def receive_audio(
