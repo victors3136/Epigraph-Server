14e1a4d8b6e5ca21a51c89afbc8a690dce9c3a37
Victor Selegean <victor.selegean@garmin.com>
2025-06-13 12:55:58 +0300

    Separate functions for testing
    Change-Id: I640b7c45ada96feb8cce6cada3bac3e8e14b126c

--- main.py
+++ main.py
@@ -17,6 +17,38 @@ max_duration_s = 30
 
 s3_client = boto3.client("s3", region_name=region_name)
 
+def extract_file_ext(file: UploadFile) -> str:
+ return os.path.splitext(file.filename or "audio.m4a")[-1].lower()
+
+def file_ext_is_valid(file_ext: str, allowed_extensions: list[str]) -> bool:
+    return file_ext.lower() in allowed_extensions
+
+def get_audio_duration(contents: bytes, format: str = "m4a") -> float:
+    audio = AudioSegment.from_file(BytesIO(contents), format=format)
+    return len(audio) / 1000.0 
+
+def is_audio_duration_longer_than_allowed(contents: bytes) -> tuple[bool, float]:
+    duration = get_audio_duration(contents)
+    return duration > max_duration_s, duration
+
+def is_consent_given(consent: str) -> bool:
+    return consent.strip().lower() == "true"
+
+
+def store_audio_to_s3(
+    filename: str,
+    contents: bytes,
+    content_type: str | None,
+    metadata: dict
+) -> None:
+    s3_client.put_object(
+        Bucket=bucket_name,
+        Key=filename,
+        Body=contents,
+        ContentType=content_type,
+        Metadata={key.lower(): str(value).lower() for key, value in metadata.items()}
+    )
+
 
 @app.post("/upload-audio/")
 async def receive_audio(
@@ -26,41 +58,35 @@ async def receive_audio(
     consent: str = Form(...)
 ) -> PlainTextResponse:
 
-    file_ext = os.path.splitext(file.filename or "audio.m4a")[-1].lower()
-    if file_ext not in allowed_extensions:
+    file_ext = extract_file_ext(file)
+
+    if not file_ext_is_valid(file_ext, allowed_extensions):
         return PlainTextResponse(
-            "Only .m4a files are allowed.",
-            status_code=status.HTTP_400_BAD_REQUEST
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content="Only .m4a files are allowed.",
         )
 
     unique_filename = f"{uuid.uuid4()}{file_ext}"
     contents = await file.read()
 
     try:
-        audio = AudioSegment.from_file(BytesIO(contents), format="m4a")
-        audio_duration_s = len(audio) / 1000.0
-        if audio_duration_s > max_duration_s:
+        overboard, duration = is_audio_duration_longer_than_allowed(contents)
+        if overboard:
             return PlainTextResponse(
-                f"Audio too long ({audio_duration_s:.1f}s). Max is 30s.",
-                status_code=status.HTTP_400_BAD_REQUEST
+                status_code=status.HTTP_400_BAD_REQUEST,
+                content=f"Audio too long ({duration:.1f}s). Max is 30s.",
             )
     except Exception as e:
         return PlainTextResponse(
-            f"Invalid audio file: {str(e)}",
-            status_code=status.HTTP_400_BAD_REQUEST
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content=f"Invalid audio file: {str(e)}"
         )
 
-    if consent.lower() == "true":
-        s3_client.put_object(
-            Bucket=bucket_name,
-            Key=unique_filename,
-            Body=contents,
-            ContentType=file.content_type,
-            Metadata={
+    if is_consent_given(consent):
+        store_audio_to_s3(unique_filename, contents, file.content_type, {
                 "age": age.lower(),
                 "gender": gender.lower()
-            }
-        )
+            })
 
     try:
         async with httpx.AsyncClient() as client:
@@ -70,13 +96,13 @@ async def receive_audio(
             transcription = response.text
     except httpx.HTTPStatusError as e:
         return PlainTextResponse(
-            f"Transcriber responded with error: {e.response.text}",
-            status_code=e.response.status_code
+            status_code=e.response.status_code,
+            content=f"Transcriber responded with error: {e.response.text}",
         )
     except Exception as e:
         return PlainTextResponse(
-            f"Transcription failed: {str(e)}",
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            content=f"Transcription failed: {str(e)}",
         )
 
-    return PlainTextResponse(transcription, status_code=status.HTTP_200_OK)
+    return PlainTextResponse(status_code=status.HTTP_200_OK, content=transcription)
