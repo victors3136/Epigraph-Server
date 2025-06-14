bbb129e74043e3425a35dc88c4c56d741ed6d5e1
victors3136 <victor.selegean@gmail.com>
2025-06-14 21:32:23 +0300

    Day's work

--- utils/utils.py
+++ utils/utils.py
@@ -1,13 +1,12 @@
-
 from fastapi import UploadFile
 from io import BytesIO
-from constants import bucket_name, max_duration_s, s3_client
+from constants import bucket_name, max_duration_s, s3_client, valid_genders
 import os
 from pydub import AudioSegment
 
 
 def extract_file_ext(file: UploadFile) -> str:
- return os.path.splitext(file.filename or "audio.m4a")[-1].lower()
+    return os.path.splitext(file.filename or "audio.m4a")[-1].lower()
 
 
 def file_ext_is_valid(file_ext: str, allowed_extensions: list[str]) -> bool:
@@ -31,11 +30,12 @@ def is_consent_given(consent: str) -> bool:
 def is_gender_valid(gender: str) -> bool:
     return gender.strip().lower() in valid_genders
 
+
 def store_audio_to_s3(
-    filename: str,
-    contents: bytes,
-    content_type: str | None,
-    metadata: dict
+        filename: str,
+        contents: bytes,
+        content_type: str | None,
+        metadata: dict
 ) -> None:
     s3_client.put_object(
         Bucket=bucket_name,
@@ -44,6 +44,7 @@ def store_audio_to_s3(
         ContentType=content_type,
         Metadata={key.lower(): str(value).lower() for key, value in metadata.items()}
     )
+
 
 def get_wav_bytes_from_m4a(contents: bytes) -> bytes:
     audio = AudioSegment.from_file(BytesIO(contents), format="m4a")
