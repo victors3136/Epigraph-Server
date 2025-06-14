ebdbd2bcb818658de94a4c44459bb26c4e29022e
victors3136 <victor.selegean@gmail.com>
2025-06-14 17:16:07 +0300

    Add aws auth env vars

--- constants.py
+++ constants.py
@@ -6,7 +6,10 @@ bucket_name = os.getenv('BUCKET_NAME')
 inference_endpoint = os.getenv('INFERENCE_ENDPOINT')
 max_duration_s = 30
 region_name = "eu-north"
-s3_client = boto3.client("s3", region_name=region_name)
+s3_client = boto3.client("s3",
+						 aws_access_key_id=os.getenv('AWS_ACC_KEY'),
+						 aws_secret_access_key=os.getenv('AWS_SEC_ACC_KEY'),
+						 region_name=region_name)
 headers = {
 	"Accept" : "application/json",
 	"Authorization": f"Bearer {os.getenv('HF_TOKEN')}",
